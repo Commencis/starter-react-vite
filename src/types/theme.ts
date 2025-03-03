a50c4d7d606322027f5dd1a3f8c6874378da26ee
Ahmet Batur Tülek <batur.tulek@commencis.com>
2025-03-03 22:34:25 +0300

    refactor: rename base color to color tint

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,13 +1,13 @@
 export type Theme = 'light' | 'dark';
 
 type Color<T extends string | number> = Record<T, string>;
-type BaseColorStep = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
+type ColorTint = 50 | 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
 
 export type ColorPalette = {
-  primary: Color<BaseColorStep>;
-  success: Color<BaseColorStep>;
-  warning: Color<BaseColorStep>;
-  error: Color<BaseColorStep>;
-  information: Color<BaseColorStep>;
-  neutral: Color<BaseColorStep | 'white' | 'black'>;
+  primary: Color<ColorTint>;
+  success: Color<ColorTint>;
+  warning: Color<ColorTint>;
+  error: Color<ColorTint>;
+  information: Color<ColorTint>;
+  neutral: Color<ColorTint | 'white' | 'black'>;
 };
