33e43896c07ee2f6dd9d2f7d76d8c0a342b80c4e
Mehmetcan Yılmaz <github@mcan.me>
2025-07-07 22:22:54 +0300

    feat: migrate storybook to v9

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,10 +3,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(ts|tsx)'],
   addons: [
-    '@storybook/addon-essentials',
     '@storybook/addon-links',
-    '@storybook/addon-actions',
-    '@storybook/addon-interactions',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
   ],
