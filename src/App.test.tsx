aaf4e2f9d0ba20bb911ab8683d98f04800d33b56
Mehmetcan Yılmaz <github@mcan.me>
2026-02-22 16:23:27 +0300

    chore: update dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,6 @@ import { render } from '@/utils/test';
 
 import App from '@/App';
 
-// Remove this after fixing globals in eslint-config
-// eslint-disable-next-line no-undef
 test('should render App component', () => {
   render(<App />);
 });
