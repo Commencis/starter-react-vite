ec922bcac4457fe7c1bb00c96ffed6bcfde83dd9
Ahmet Batur Tülek <batur.tulek@commencis.com>
2026-07-06 16:09:15 +0300

    chore: update prettier to 3.9.4

--- src/components/ui/Icon/Icon.types.ts
+++ src/components/ui/Icon/Icon.types.ts
@@ -1,7 +1,4 @@
 export type IconSize = 12 | 14 | 16 | 18 | 20 | 22 | 24 | 32 | 40;
 
 export type IconName =
-  | 'chevron-right'
-  | 'chevron-left'
-  | 'chevron-up'
-  | 'chevron-down';
+  'chevron-right' | 'chevron-left' | 'chevron-up' | 'chevron-down';
